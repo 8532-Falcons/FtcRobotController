1d7ddbfc459c6954a42f799ab386cf56496d8193
= <25adayasankar@frcsfalcons.org>
2024-11-04 12:24:30 -0500

    Add autonomous code, set up initialization & motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCode.java
@@ -20,6 +20,7 @@ public class AutoCode extends LinearOpMode {
         Initialization period
         Motors are fetched from hardwareMap
          */
+
         frontLeft = hardwareMap.dcMotor.get("Front Left");
         frontRight = hardwareMap.dcMotor.get("Front Right");
         backLeft = hardwareMap.dcMotor.get("Back Left");

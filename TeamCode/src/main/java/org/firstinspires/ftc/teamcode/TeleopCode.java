679db9b1479620c658dd973aabeccb10423400ca
= <25adayasankar@frcsfalcons.org>
2024-11-27 22:49:04 -0500

    Mecanum Controller implemented in Teleop & gamepad

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopCode.java
@@ -24,11 +24,15 @@ public class TeleopCode extends LinearOpMode {
         frontRight = hardwareMap.dcMotor.get("Front Right");
         backLeft = hardwareMap.dcMotor.get("Back Left");
         backRight = hardwareMap.dcMotor.get("Back Right");
+        MecanumController mecControl = new MecanumController(frontLeft, frontRight, backLeft,
+                backRight);
 
         waitForStart();
 
         while (opModeIsActive()) {
-
+            // Moves towards
+            mecControl.moveBot1(Math.atan2(gamepad1.right_stick_y, gamepad1.right_stick_x),
+                    Math.hypot(Math.abs(gamepad1.right_stick_x), Math.abs(gamepad1.right_stick_y)));
         }
     }
 }

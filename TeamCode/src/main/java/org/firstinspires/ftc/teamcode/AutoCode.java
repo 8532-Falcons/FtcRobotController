0f810dc418aa80d697b826eda5c520873bd9f359
= <25adayasankar@frcsfalcons.org>
2024-12-26 13:51:58 -0500

    Reversed right wheel motors, added notes for devs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCode.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 /**
  * This code is the main central autonomous code! The code in this file will run once after the code
@@ -19,17 +20,21 @@ public class AutoCode extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        /*
-        Initialization period
-        Motors are fetched from hardwareMap
-         */
-        frontLeft = hardwareMap.dcMotor.get("Front Left");
-        frontRight = hardwareMap.dcMotor.get("Front Right");
-        backLeft = hardwareMap.dcMotor.get("Back Left");
-        backRight = hardwareMap.dcMotor.get("Back Right");
+        // INITIALIZATION PERIOD - RUNS ONCE AFTER INIT BUTTON
+        // Motors are fetched from hardwareMap
+        frontLeft = hardwareMap.dcMotor.get("Front Left"); // front-left wheel
+        frontRight = hardwareMap.dcMotor.get("Front Right"); // front-right wheel
+        backLeft = hardwareMap.dcMotor.get("Back Left"); // back-left wheel
+        backRight = hardwareMap.dcMotor.get("Back Right"); // back-right wheel
 
+        // New mecanum controller created using motors
+        MecanumController mecControl = new MecanumController(frontLeft, frontRight, backLeft,
+                backRight);
+
+        // Op mode waits for the play button to be pressed
         waitForStart();
 
-
+        // TELEOP PERIOD - CODE RUNS ONCE
+        // TODO: Implement autonomous code
     }
 }
